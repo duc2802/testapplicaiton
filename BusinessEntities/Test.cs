8499065c66ab530f40e4a8b48abe55a7edb40bef
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-04-04 15:56:28 +0000

    Update config

--- BusinessEntities/Test.cs
+++ BusinessEntities/Test.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/**-----------------------------------------------------------
+* Program : TestApplication
+* Written by : tiendv 
+* Email : tiendo.vn@gmail.com
+•	Created date: 4/4/2013 
+* Modified by: Nguyen Van B
+•	Modified date:
+•	Version: 1.0	
+•	Description: 
+----------------------------------------------------------*/ 
+
 namespace BusinessEntities
 {
     class Test
