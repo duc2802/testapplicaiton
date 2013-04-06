7d4cd75a78bcc1380a80cd5a22a369d1eb2ee179
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-04-06 15:11:28 +0000

    Add welcomeScreen

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace TestApplicaiton
+namespace TestApplication
 {
     static class Program
     {
--- PresentationLayer/WelcomeScreen.designer.cs
+++ PresentationLayer/WelcomeScreen.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PresentationLayer
 {
-    partial class Main
+    partial class WelcomeScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
-            // Main
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 420);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(563, 32);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Welcome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 296);
-            this.Name = "Main";
-            this.Text = "Main";
+            this.BackgroundImage = global::PresentationLayer.Properties.Resources.Welcome;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(563, 452);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Welcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome";
+            this.TopMost = true;
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
