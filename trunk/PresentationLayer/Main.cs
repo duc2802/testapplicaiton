20b6930442a66862200c4e2c7173c3980d75e3a1
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-04-06 15:11:28 +0000

    Add welcomeScreen

--- trunk/PresentationLayer/Main.cs
+++ trunk/PresentationLayer/Main.cs
@@ -6,14 +6,58 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using TestApplication;
 
 namespace PresentationLayer
 {
     public partial class Main : Form
     {
+        private  WelcomeScreen welcomeScreen;
+        private bool done = false;
+
         public Main()
         {
             InitializeComponent();
+            this.Load += new EventHandler(HandleFormLoad);
+            this.welcomeScreen = new WelcomeScreen();
         }
+
+        // WelcomeScreen controler
+        private void HandleFormLoad(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            Thread thread = new Thread(new ThreadStart(this.ShowSplashScreen));
+            thread.Start();
+
+            Hardworker worker = new Hardworker();
+            worker.ProgressChanged += (o, ex) =>
+            {
+                this.welcomeScreen.UpdateProgress(ex.Progress);
+            };
+
+            worker.HardWorkDone += (o, ex) =>
+            {
+                done = true;
+                this.Show();
+            };
+
+            worker.DoHardWork();
+        }
+
+
+
+        private void ShowSplashScreen()
+        {
+            welcomeScreen.Show();
+            while (!done)
+            {
+                Application.DoEvents();
+            }
+            welcomeScreen.Close();
+            this.welcomeScreen.Dispose();
+        }
+
     }
 }
---
+++ trunk/PresentationLayer/WelcomeScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using System.Linq;
+
+namespace PresentationLayer
+{
+    public partial class WelcomeScreen : Form
+    {
+        private delegate void ProgressDelegate(int progress);
+
+        private ProgressDelegate del;
+        public WelcomeScreen()
+        {
+            InitializeComponent();
+            this.progressBar1.Maximum = 100;
+            del = this.UpdateProgressInternal;
+        }
+
+        private void UpdateProgressInternal(int progress)
+        {
+            if (this.Handle == null)
+            {
+                return;
+            }
+
+            this.progressBar1.Value = progress;
+        }
+
+        public void UpdateProgress(int progress)
+        {
+            this.Invoke(del, progress);
+        }
+    }
+}
