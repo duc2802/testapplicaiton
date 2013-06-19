9de42c0241c9c3fe8b6281497f5b771e95a57cc4
duc2802@gmail.com <duc2802@gmail.com@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-06-19 21:27:01 +0000

    Add more code for client.

---
+++ trunk/ClientPresentationLayer/QuestionPresentation/TestManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClientPresentationLayer.Common;
+using ClientPresentationLayer.QuestionPresentation.Data;
+using Commons.BusinessObjects;
+using SingleInstanceObject;
+
+namespace ClientPresentationLayer.QuestionPresentation
+{
+    public partial class TestManager : UserControl
+    {
+        private TestDataListViewItemController _dataController;
+        private TestDataListViewItemController DataController
+        {
+            set
+            {
+                _dataController = value;
+                OnChangeDataController();
+            }
+            get { return _dataController; }
+        }
+
+        public TestManager()
+        {
+            InitializeComponent();
+            Init();
+            InitGui();
+            InitEvent();
+        }
+
+        private void Init()
+        {
+            DataController = new TestDataListViewItemController();
+        }
+
+        private void InitGui()
+        {
+            testlistView.Items.AddRange(DataController.DisplayItems.ToArray());
+        }
+
+        private void InitEvent()
+        {
+            testlistView.SelectedIndexChanged += TestlistViewSelectedIndexChanged;
+            startExamButton.Click += StartExamButtonClick;
+        }
+
+        #region implement Event
+
+        private void TestlistViewSelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (testlistView.SelectedItems.Count == 0)
+            {
+                deleteButton.Enabled = false;
+                startExamButton.Enabled = false;
+                return;
+            }
+            var itemSelected = testlistView.SelectedItems[0];
+            if(itemSelected != null)
+            {
+                Singleton<DataItemCollection>.Instance.TestItemDataSelected = TryGetDataItem((int)itemSelected.Tag);
+                deleteButton.Enabled = true;
+                startExamButton.Enabled = true;
+            }
+        }
+
+        private void StartExamButtonClick(object sender, EventArgs e)
+        {
+            OnStartExam();
+        }
+
+        #endregion
+
+        private TestDataListViewItem TryGetDataItem(int id)
+        {
+            return DataController.DataItems.FirstOrDefault(dataItem => dataItem.Id == id);
+        }
+
+        private void OnChangeDataController()
+        {
+            numOfExamTextBox.Text = DataController.DataItems.Count.ToString();
+        }
+
+        #region Register new event
+
+        public event ActionEventHandler StartExam
+        {
+            add
+            {
+                lock (_startExamEventLocker)
+                {
+                    _startExamEvent += value;
+                }
+            }
+            remove
+            {
+                lock (_startExamEventLocker)
+                {
+                    _startExamEvent -= value;
+                }
+            }
+        }
+
+        private ActionEventHandler _startExamEvent;
+        private readonly object _startExamEventLocker = new object();
+
+        private void OnStartExam()
+        {
+            ActionEventHandler handler = _startExamEvent;
+            if (handler != null)
+            {
+                try
+                {
+                    handler(this);
+                }
+                catch (Exception ex)
+                {
+                    //Log
+                }
+            }
+        }
+
+        #endregion End register new event
+    }
+}
