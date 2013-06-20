d31d0203ade3b8bdb43a4c2b5d90370c9e85e0f4
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-06-20 15:39:27 +0000

    action when button export click

--- TestApplication/TestBLL.cs
+++ TestApplication/TestBLL.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BusinessEntities;
+using DataAccessLayer;
 
 namespace TestApplication
 {
-    class TestBLL
+    public class TestBLL
     {
+        public bool exportTestXMLFile(TestBE testData, string name, string placeToSave)
+        {
+            bool result = ActionXML.saveExam(testData,name,placeToSave);
+            return result;
+        }
     }
 }
