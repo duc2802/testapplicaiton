e20d7cf032e0dad4954857caf5272ec289e736ec
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-06-22 17:55:15 +0000

    Add item for load

---
+++ ClientPresentationLayer/QuestionPresentation/Data/QuestionDataController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Commons.BusinessObjects;
+
+namespace ClientPresentationLayer.QuestionPresentation.Data
+{
+    public class QuestionDataController
+    {
+        public List<QuestionDataItem> DataItems = new List<QuestionDataItem>();
+    }
+        
+}
---
+++ ClientPresentationLayer/QuestionPresentation/Data/QuestionDataItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using BusinessEntities;
+using PropertyChangedEventHandler = Commons.BusinessObjects.PropertyChangedEventHandler;
+
+namespace ClientPresentationLayer.QuestionPresentation.Data
+{
+    public class QuestionDataItem
+    {
+        public int IdQuestion { set; get; }
+        public string Explain { set; get; }
+        public string ImageName { set; get;}
+        public string IsDone { set; get; }
+        public string IsTrue { set; get; }
+    }
+}
