62ab3d3cd5b887e0bea38be650f9733786ef897c
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-07-18 11:51:28 +0000

    insert code call Editor

---
+++ TestCallEditor/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TestCallEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            // _contentQuestionTextEditor.InsertImage();
+            string editor = path + "\\" + "Editor.exe";
+
+            Process p = new Process();
+            p.StartInfo.FileName = editor;
+            p.Start();
+            string test = p.ProcessName;
+            p.WaitForExit();
+        }
+    }
+}
