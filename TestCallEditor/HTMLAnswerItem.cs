d2db1fe11738dc0c0c20ee6021c3ded53d75c923
tiendo.vn <tiendo.vn@3282097a-bfc2-53e3-59ec-92d816c580f1>
2013-07-19 17:46:06 +0000

    Commit

---
+++ TestCallEditor/HTMLAnswerItem.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Commons.BusinessObjects;
+using Commons.CommonGui;
+using LiveSwitch.TextControl;
+using TextEditor = LiveSwitch.TextControl.Editor;
+
+namespace TestCallEditor
+{
+    public partial class HTMLAnswerItem : UserControl
+    {
+        public TextEditor contentAnswerTextEditor;
+        private HtmlRichTextBox htmlRichTextBox;
+
+        private AnswerDataItem _dataItem;
+        public AnswerDataItem DataItem
+        {
+            set
+            {
+                _dataItem = value;
+                Name = _dataItem.orderAnswer.ToString();
+                //OnDataItemChanged();
+            }
+            get { return _dataItem; }
+        }
+
+        public HTMLAnswerItem()
+        {
+            InitializeComponent();
+            InitCustomComponent(true);
+            InitEvent();
+        }
+
+        public HTMLAnswerItem(AnswerDataItem itemData, bool isEditMode)
+        {
+            InitializeComponent();
+            InitCustomComponent(isEditMode);
+            InitGui(itemData, isEditMode);
+            //InitEvent();
+            InitData(itemData);
+        }
+
+        public HTMLAnswerItem(int index)
+        {
+            InitializeComponent();
+            InitCustomComponent(true);
+            InitEvent();
+            InitData(index);
+        }
+
+        public HTMLAnswerItem(AnswerDataItem item, int index)
+        {
+            InitializeComponent();
+            InitCustomComponent(true);
+            InitData(index);
+            InitEvent();
+        }
+
+        private void InitGui(AnswerDataItem itemData, bool isEditMode)
+        {
+            orderAnswerLabel.Text = (itemData.OrderAnswer + 1).ToString();
+            if (isEditMode)
+            {
+                contentAnswerTextEditor.Html = itemData.ContentAnswer;
+            }
+            else
+            {
+                htmlRichTextBox.AddHTML(itemData.ContentAnswer);
+                htmlRichTextBox.Text = htmlRichTextBox.Text.Trim();
+            }
+        }
+
+        private void InitEvent()
+        {
+            deleteButton.Click += DeleteAnswerButtonClick;
+            Leave += ItemLeave;
+            trueCheckBox.CheckedChanged += TrueCheckBoxCheckedChanged;
+        }
+
+        private void ItemLeave(object sender, EventArgs e)
+        {
+            _dataItem.ContentAnswer = contentAnswerTextEditor.DocumentText;
+        }
+
+        private void InitData(int index)
+        {
+            DataItem = new AnswerDataItem();
+            orderAnswerLabel.Text = index.ToString();
+            //DataItem.PropertyChanged += DataItemPropertyChanged;
+            Refresh();
+        }
+
+        private void InitData(AnswerDataItem data)
+        {
+            _dataItem = data;
+        }
+
+        private void InitCustomComponent(bool isEditMode)
+        {
+            SuspendLayout();
+            //Init contentQuestionTextEditor
+            contentPanel.BorderStyle = BorderStyle.None;
+            if(isEditMode)
+            {
+                contentAnswerTextEditor = new TextEditor(isEditMode);
+                contentAnswerTextEditor.BackColor = SystemColors.Control;
+                contentAnswerTextEditor.BodyBackgroundColor = Color.White;
+                contentAnswerTextEditor.BodyHtml = null;
+                contentAnswerTextEditor.Parent = this;
+                contentAnswerTextEditor.Parent = this;
+                contentAnswerTextEditor.BodyText = null;
+                contentAnswerTextEditor.Dock = DockStyle.Fill;
+                contentAnswerTextEditor.EditorBackColor = Color.FromArgb(((((255)))), ((((255)))), ((((255)))));
+                contentAnswerTextEditor.EditorForeColor = Color.FromArgb(((((0)))), ((((0)))), ((((0)))));
+                contentAnswerTextEditor.FontSize = FontSize.Three;
+                contentAnswerTextEditor.Html = null;
+                contentAnswerTextEditor.Location = new Point(0, 24);
+                contentAnswerTextEditor.Name = "editor";
+                contentAnswerTextEditor.Size = new Size(632, 124);
+                contentAnswerTextEditor.TabIndex = 1;
+                contentPanel.Controls.Add(contentAnswerTextEditor);
+            }
+            else
+            {
+                htmlRichTextBox = new HtmlRichTextBox();
+                htmlRichTextBox.Dock = DockStyle.Fill;
+                htmlRichTextBox.BorderStyle = BorderStyle.None;
+                htmlRichTextBox.Name = "htmlRichTextBox";
+                htmlRichTextBox.Size = new Size(369, 30);
+                htmlRichTextBox.TabIndex = 1;
+                contentPanel.Controls.Add(htmlRichTextBox);
+            }
+
+            if (!isEditMode)
+            {
+                trueCheckBox.Visible = false;
+                deleteButton.Visible = false;
+                orderAnswerLabel.BorderStyle = BorderStyle.None;
+                BorderStyle = BorderStyle.None;
+
+                contentPanel.Size = new Size(369, 30);
+            }
+            ResumeLayout();
+        }
+
+        private void TrueCheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+            DataItem.isTrue = trueCheckBox.Checked;
+            //OnCheckChange(DataItem.orderAnswer - 1, DataItem.isTrue);  
+        }
+
+        private void DeleteAnswerButtonClick(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(this, "Do you want to delete this question?", "Delete question.",
+                                         MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                OnDelete(this.DataItem.orderAnswer);
+            }
+        }
+
+        public event ActionEventHandler<int> Delete
+        {
+            add
+            {
+                lock (_deleteEventLocker)
+                {
+                    _deleteEvent += value;
+                }
+            }
+            remove
+            {
+                lock (_deleteEventLocker)
+                {
+                    _deleteEvent -= value;
+                }
+            }
+        }
+
+        private ActionEventHandler<int> _deleteEvent;
+        private readonly object _deleteEventLocker = new object();
+
+        private void OnDelete(int idAnswer)
+        {
+            ActionEventHandler<int> handler = _deleteEvent;
+            if (handler != null)
+            {
+                try
+                {
+                    handler(this, idAnswer);
+                }
+                catch (Exception ex)
+                {
+                    //Log
+                }
+            }
+        }
+    }
+}
---
+++ TestCallEditor/QuestionDataItem.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BusinessEntities;
+using PropertyChangedEventHandler = Commons.BusinessObjects.PropertyChangedEventHandler;
+
+namespace TestCallEditor
+{
+    public class QuestionDataItem
+    {
+        private AnswerDataController _answers = new AnswerDataController();
+        private string _contentQuestion;
+        private int _orderQuestion;
+        public int Height { set; get; }
+        public string IdQuestion { set; get; }
+        public string ExplainContent { set; get; }
+        public string imageName { set; get; }
+
+        public int OrderQuestion
+        {
+            set
+            {
+                _orderQuestion = value;
+                OnPropertyChanged("OrderQuestion");
+            }
+            get { return _orderQuestion; }
+        }
+
+        public string ContentQuestion
+        {
+            set
+            {
+                _contentQuestion = value;
+                OnPropertyChanged("ContentQuestion");
+            }
+            get { return _contentQuestion; }
+        }
+
+        public AnswerDataController AnswerData
+        {
+            set { _answers = value; }
+            get { return _answers; }
+        }
+
+        public QuestionBE getQuestionBE()
+        {
+            var qBe = new QuestionBE();
+            qBe.QuestionID = IdQuestion;
+            qBe.QuestionContent = ContentQuestion;
+            qBe.Explain = ExplainContent;
+            qBe.ListAnswers = new List<AnswerBE>();
+            foreach (AnswerDataItem answer in AnswerData.AnswerData)
+            {
+                var answerBe = new AnswerBE();
+                answerBe.AnswerID = answer.orderAnswer.ToString();
+                answerBe.Content = answer.ContentAnswer;
+                if (answer.isTrue)
+                {
+                    answerBe.Result = "1";
+                }
+                else
+                {
+                    answerBe.Result = "0";
+                }
+                qBe.ListAnswers.Add(answerBe);
+            }
+
+            return qBe;
+        }
+
+        #region Trigger Event
+
+        private readonly object _propertyChangedEventLocker = new object();
+        private PropertyChangedEventHandler _propertyChangedEvent;
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add
+            {
+                lock (_propertyChangedEventLocker)
+                {
+                    _propertyChangedEvent += value;
+                }
+            }
+            remove
+            {
+                lock (_propertyChangedEventLocker)
+                {
+                    _propertyChangedEvent -= value;
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler;
+            lock (_propertyChangedEventLocker)
+                handler = _propertyChangedEvent;
+
+            if (handler == null)
+            {
+                return;
+            }
+            try
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+            catch (Exception ex)
+            {
+                //Log here
+            }
+        }
+
+        #endregion
+    }
+}
